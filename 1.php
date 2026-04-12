c4c4bf6ad0b26385da030aef53a583ed04cb2d35
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-04-12 13:11:54 +0530

    basics of PHP

--- 1.php
+++ 1.php
@@ -1,4 +1,15 @@
 <?php
  echo"hello world";
- echo "<h1>this is heading 1</h1>"
+ echo "<h1>this is heading 1</h1>";
+// variable in php 
+$name = "Sarthak";
+echo $name;
+$age = 21; 
+echo "$name = name and age is $age"; // variable interpolation. this means to write multiple variable in one line .
+
+$name1 ="sparsh";
+echo $name1.$name;
+$a = 10;
+$b = 20;    
+echo "sum = ".$a +$b;
  ?>
