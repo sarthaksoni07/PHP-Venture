920f1b18c2dfc619762e2261b2fa992a1d5f0b5a
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-04-12 14:44:34 +0530

    for while and do while loop

--- 3.php
+++ 3.php
@@ -1,4 +1,16 @@
 <!-- loop -->
  <?php
- echo "this is file 3"
+ echo "this is file 3";
+ echo "<br>";
+ for($i = 1 ; $i <=5;$i++){
+    for($j = 1; $j <=$i; $j++){
+        echo "*";
+    }
+    echo "<br>";
+ }
+$a=10;
+ while ($a > 1){
+    echo $a;
+    $a-=1;
+ }
  ?>
