ec79531e3b12bc9477dac87783ffeb0d603d9c05
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-04-27 21:41:19 +0530

    database connection, validatoin , error handling and query

--- 9.php
+++ 9.php
@@ -1,22 +1,46 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","test");
-if($conn){
-    echo "successfull<br>";
+try{
+    $conn = mysqli_connect("localhost","root","","test");
+    if(!$conn){
+    throw new Exception("connection failed");
+    }
+    echo "successfull connected to database<br>";
 }
+catch(Exception $e){
+   echo $e->getmessage();
+}
+
+
 $age  = $_POST['age'];
 $name  = trim($_POST['name']);
 $sname  = trim($_POST['sname']);
 
-if(empty($age)|| empty($name)||empty($sname)){
-    die("values cannot be empty<br>");
+t ry{
+
+    if(empty($age)|| empty($name)||empty($sname)){
+        throw new Exception("values cannot be empty<br>");
+    }
+
+    $sql = "INSERT INTO students (age,name,sname) VALUES ($age , '$name', '$sname')";
+
+    if(!mysqli_query($conn, $sql)){
+        throw new Exception(mysqli_error($conn));
+    };
+
+    echo "successfully submitted<br>";
+}
+catch(Exception $e){
+    echo $e->getmessage();
 }
 
-$sql = "INSERT INTO students (age,name,sname) VALUES ($age , '$name', '$sname')";
-if(mysqli_query($conn, $sql)){
-    echo "successful<br>";
+try{
+    if(!mysqli_close($conn)){
+        throw new Exception("failed to close");
+    }
+    echo "connection closed successfully";
 }
-else{
-    echo "unsucessful<br>";
+catch(Exception $e){
+   echo $e->getmessage();
 }
 ?>
